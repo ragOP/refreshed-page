87b09c41c22e67c5e127d6a976c92a9dbfbf25a3
ragOP <najmi.raghib@gmail.com>
2024-07-15 17:15:16 +0530

    ds

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,17 @@
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import './App.scss';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.scss";
 
-import First_EN from './pages/1_en';
-import First_SP from './pages/1_sp';
-import Second_EN from './pages/2_en';
-import Second_SP from "./pages/2_sp";
-import Third_EN from './pages/3_en';
-import Third_SP from './pages/3_sp';
-import NotFound from './pages/404';
-import Forth_EN from './pages/4_en';
-import Forth_SP from './pages/4_sp';
-import Fifth_EN from './pages/5_en';
-import Fifth_SP from './pages/5_sp';
-import Sixth_SP from './pages/6_en';
-import S from './pages/7'
-import New from './pages/8'
-import Urgent from './pages/9'
-import VeryNew from './pages/10'
-import Visits from "./pages/visits";
-import Test from './pages/111';
-import Client from './pages/Client'
-import Ip from './pages/ip'
-import Newx from './pages/Newx'
-import Party from './pages/Party';
-import Food from './pages/Food';
-import Late from './pages/Late';
+import VeryNew from "./pages/10";
+
+import Late from "./pages/Late";
 
 function App() {
-
   return (
-    
     <Router>
       <div className="App">
         <Routes>
-        
-         <Route path = "/engmedssd" element = {<Late/>} />
-          <Route path = "/engmed900" element = {<VeryNew />} />
-
-
-      
+          <Route path="/ssdengmed" element={<Late />} />
+          <Route path="/900engmed" element={<VeryNew />} />
         </Routes>
       </div>
     </Router>
