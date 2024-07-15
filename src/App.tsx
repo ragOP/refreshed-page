80d420094aee12b315e7310d6ada0d5dfb226ed2
ragOP <najmi.raghib@gmail.com>
2024-07-15 19:04:41 +0530

    fdfd

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "./App.scss";
 import VeryNew from "./pages/10";
 
 import Late from "./pages/Late";
+import Terms from "./pages/Terms";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/ssdengmed" element={<Late />} />
           <Route path="/900engmed" element={<VeryNew />} />
+          <Route path="/terms-and-conditions" element={<Terms />} />
         </Routes>
       </div>
     </Router>
