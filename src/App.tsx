efb8e587df193631af1b26e9776e4eb4dc890421
farish-jamal <farishjamal98@gmail.com>
2024-07-18 22:35:57 +0530

    added

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import VeryNew from "./pages/10";
 import Late from "./pages/Late";
 import Terms from "./pages/Terms";
 import Privacy from "./pages/Privacy";
+import Ww from "./pages/ww"
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/ssdengmed" element={<Late />} />
           <Route path="/900engmed" element={<VeryNew />} />
+          <Route path="/engmed900" element={<Ww />} />
           <Route path="/terms-and-conditions" element={<Terms />} />
           <Route path="/privacy-policy" element={<Privacy />} />
         </Routes>
