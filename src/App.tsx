cbbc7a72fd288439bbe0df824f6698e39171bd75
farish-jamal <farishjamal98@gmail.com>
2024-07-24 20:43:30 +0530

    sdfjhdfs

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Late from "./pages/Late";
 import Terms from "./pages/Terms";
 import Privacy from "./pages/Privacy";
 import Ww from "./pages/ww"
+import VeryNew1 from "./pages/11111"
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/ssdengmed" element={<Late />} />
           <Route path="/900engmed" element={<VeryNew />} />
+          <Route path="/" element={<VeryNew1 />} />
           <Route path="/engmed900" element={<Ww />} />
           <Route path="/terms-and-conditions" element={<Terms />} />
           <Route path="/privacy-policy" element={<Privacy />} />
