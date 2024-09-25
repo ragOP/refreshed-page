c01b7fcb5a2a38a99ceb7b56cd2421d54137c688
ragOP <najmiraghib@gmail.com>
2024-09-25 23:49:23 +0530

    css

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Terms from "./pages/Terms";
 import Privacy from "./pages/Privacy";
 import Ww from "./pages/ww"
 import VeryNew1 from "./pages/11111"
+import Tsf from "./pages/Tsf";
 
 function App() {
   return (
@@ -17,6 +18,8 @@ function App() {
           <Route path="/ssdengmed" element={<Late />} />
           <Route path="/900engmed" element={<VeryNew />} />
           <Route path="/" element={<VeryNew1 />} />
+          <Route path="Tsf" element={<Tsf />} />
+
           <Route path="/engmed900" element={<Ww />} />
           <Route path="/terms-and-conditions" element={<Terms />} />
           <Route path="/privacy-policy" element={<Privacy />} />
